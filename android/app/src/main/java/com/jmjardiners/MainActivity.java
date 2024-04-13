778d0ae977e94a69ba365f37ea84823187f414e4
Vinicio <calamot51@gmail.com>
2024-04-13 18:47:07 +0200

    all i sok

--- android/app/src/main/java/com/jmjardiners/MainActivity.java
+++ android/app/src/main/java/com/jmjardiners/MainActivity.java
@@ -1,4 +1,4 @@
-package com.ecommerce;
+package com.jmjardiners;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -14,7 +14,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "ecommerce";
+    return "jmjardiners";
   }
 
   /**
--- android/app/src/main/java/com/jmjardiners/MainApplication.java
+++ android/app/src/main/java/com/jmjardiners/MainApplication.java
@@ -1,4 +1,4 @@
-package com.ecommerce;
+package com.jmjardiners;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
